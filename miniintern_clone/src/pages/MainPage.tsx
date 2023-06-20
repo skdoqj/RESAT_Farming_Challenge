fdf12840f296a649fc5f729026639b6cdf13040b
suwan <skdoqj@naver.com>
2023-06-20 23:17:08 +0900

    [#2] 메인페이지 구획

--- miniintern_clone/src/pages/MainPage.tsx
+++ miniintern_clone/src/pages/MainPage.tsx
@@ -1,7 +1,95 @@
+import * as S from "../styles/mainStyle";
+import { ImageCard, LogoCard } from "../components/card";
+
 function Main() {
   return (
     <>
-      <div>Hello</div>
+      <S.Main>
+        <S.Banner>
+          <main>
+            <section className="head">
+              <div className="top_banner left_banner">
+                <div className="top_banner_text">
+                  <p>최대한의 효율로</p>
+                  <p>직무경험을 쌓아보세요</p>
+                  <p className="sub">2주만에 실무 경험을 만드는, 미니인턴</p>
+                  <span>미니인턴으로 가기</span>
+                </div>
+                <img src="https://miniintern.com/static/images/main/main_img_1.webp" />
+              </div>
+              <div className="top_banner right_banner">
+                <div className="top_banner_text">
+                  <p>미니인턴?</p>
+                  <p>미니인턴!</p>
+                  <span>서비스 더 알아보기</span>
+                </div>
+                <img src="https://miniintern.com/static/images/main/main_img_r_1.webp" />
+              </div>
+            </section>
+
+            <section className="assignment">
+              <h1>미니인턴으로 취업하세요</h1>
+              <div className="text_bottom">
+                <h2>
+                  이제 집, 학교, 도서관, 카페에서 인턴십을 경험하고,
+                  실무역량평가로 취업하세요
+                </h2>
+                <div className="go">모든 기업과제 보기</div>
+              </div>
+              <ImageCard></ImageCard>
+            </section>
+
+            <section className="slide_banner">
+              <div className="background"></div>
+              <div className="banner_container">
+                <div className="banner"></div>
+                <div className="text_box">
+                  <span>서비스 런칭</span>
+                  <p className="title">해피폴리오로 취업 정보를 받으세요</p>
+                  <p className="sub">
+                    검증된 현직자의 취업 스토리로 <br></br>취준 전략을 세우세요
+                  </p>
+                </div>
+              </div>
+            </section>
+            <section className="company">
+              <h1>채용 중인 기업을 확인하세요</h1>
+              <div className="text_bottom">
+                <h2>내게 맞는 기업을 찾았나요? 바로 지원해 보세요</h2>
+                <div className="go">모든 채용정보 보기</div>
+              </div>
+              <LogoCard></LogoCard>
+            </section>
+            <section className="bottom_banner">
+              <div className="banner">
+                <div className="banner_text">
+                  이력서 피드백도 받고 회사도 추천받고 싶다면?
+                </div>
+                <button>헤드헌팅 설정하기</button>
+                <img src="https://miniintern.com/static/images/main/headhunting_banner_pc.png" />
+              </div>
+            </section>
+            <section className="m_class">
+              <h1>M클래스를 신청하세요</h1>
+
+              <div className="text_bottom">
+                <h2>미니인턴만의 색깔을 가진 교육을 제공해 드려요</h2>
+                <div className="go">모든 M클래스 보기</div>
+              </div>
+              <ImageCard></ImageCard>
+            </section>
+            <section className="happy_folio">
+              <h1>해피폴리오로 취업 정보를 받으세요</h1>
+
+              <div className="text_bottom">
+                <h2>검증된 현직자의 취업 스토리로 취준 전략을 세우세요</h2>
+                <div className="go">모든 해피폴리오 보기</div>
+              </div>
+              <ImageCard></ImageCard>
+            </section>
+          </main>
+        </S.Banner>
+      </S.Main>
     </>
   );
 }
---
+++ miniintern_clone/src/styles/mainStyle.tsx
@@ -0,0 +1,162 @@
+import { styled } from "styled-components";
+const themeColor = "rgb(23, 161, 255)";
+
+export const Main = styled.div`
+  width: 936px;
+  margin: auto;
+  text-align: left;
+
+  section {
+    padding-top: 40px;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    font-size: 1.7rem;
+    font-weight: 400;
+    margin-bottom: 8px;
+  }
+
+  .text_bottom {
+    display: flex;
+    justify-content: space-between;
+  }
+  h2 {
+    font-size: 1rem;
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+  .go {
+    font-size: 0.9rem;
+    font-weight: 400;
+    color: ${themeColor};
+  }
+`;
+
+export const Banner = styled.div`
+  //head
+  .head {
+    display: flex;
+    justify-content: space-between;
+  }
+  .top_banner {
+    border-radius: 8px;
+    position: relative;
+    overflow: hidden;
+    height: 400px;
+  }
+  .top_banner img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+  .left_banner {
+    width: 65%;
+  }
+  .right_banner {
+    width: 32%;
+  }
+  .top_banner_text {
+    margin: 40px;
+    color: white;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+  .top_banner_text p {
+    margin-bottom: 9px;
+  }
+  .top_banner_text span {
+    font-size: 0.9rem;
+    margin-top: auto;
+    bottom: 30px;
+    position: absolute;
+  }
+  .top_banner_text > .sub {
+    font-size: 1.5rem;
+  }
+
+  //중간 배너
+  .banner_container {
+    display: flex;
+    margin-top: 20px;
+  }
+  .background {
+    background-color: rgb(240, 243, 243);
+    height: 230px;
+    width: auto;
+    position: absolute;
+    z-index: -1;
+    left: 0;
+    right: 0;
+  }
+  .slide_banner .banner {
+    width: 335px;
+    height: 250px;
+    background-color: green;
+    border-radius: 15px;
+  }
+  .slide_banner .text_box {
+    padding-top: 70px;
+    padding-left: 30px;
+    position: relative;
+  }
+  .slide_banner .text_box span {
+    position: absolute;
+    top: 20px;
+    left: 0px;
+    background-color: ${themeColor};
+    color: white;
+    padding: 10px 25px 10px 30px;
+    font-weight: 800;
+    font-size: 0.8rem;
+    border-radius: 0 20px 20px 0;
+  }
+  .slide_banner .text_box .title {
+    font-size: 1.1rem;
+    font-weight: 400;
+    color: gray;
+    margin-bottom: 10px;
+  }
+  .slide_banner .text_box .sub {
+    font-size: 1.4rem;
+    font-weight: 700;
+    line-height: 30px;
+  }
+
+  //하단배너
+  .bottom_banner .banner {
+    position: relative;
+    width: 100%;
+    height: 120px;
+    border-radius: 5px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .bottom_banner .banner img {
+    position: absolute;
+    width: 100%;
+    left: 0;
+    top: 0;
+    z-index: -1;
+  }
+
+  .bottom_banner .banner_text {
+    color: white;
+    font-size: 1.5rem;
+    font-weight: 600;
+  }
+
+  .bottom_banner button {
+    padding: 15px 20px;
+    font-size: 0.9rem;
+    font-weight: 600;
+    background-color: rgba(255, 255, 255, 0.25);
+    color: white;
+    border-radius: 30px;
+  }
+`;
