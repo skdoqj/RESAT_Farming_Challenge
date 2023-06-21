28860b4c953b8e1d7e060f740ec11eb6de85f4c3
suwan <skdoqj@naver.com>
2023-06-21 23:46:05 +0900

    [#3] 카운트다운 기능 완료

--- miniintern_clone/src/pages/TimerPage.tsx
+++ miniintern_clone/src/pages/TimerPage.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import * as S from "../styles/timerStyle";
 
 function Timer() {
   let [time, setTime] = useState({ hour: 0, minute: 0, seconds: 0 });
@@ -7,7 +8,7 @@ function Timer() {
   let [minute, setMinute] = useState(0);
   let [seconds, setSeconds] = useState(0);
 
-  let [a, setA] = useState(false);
+  let [lock, setLock] = useState(false);
 
   //input 시간
   const hourChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,11 +28,15 @@ function Timer() {
   let hourSec = hour * 3600;
   let realSec = seconds + minSec + hourSec;
 
+  let intervalID: any = useRef();
+  console.log(intervalID);
+
   //카운트 다운 함수
   const startClick = () => {
+    clearInterval(intervalID.current);
     // setTime({ ...time, hour: hhour });
-    setInterval(operatorTimer, 1000);
-    setA(!a);
+    intervalID.current = setInterval(operatorTimer, 1000);
+    setLock(true);
   };
 
   const operatorTimer = () => {
@@ -41,34 +46,56 @@ function Timer() {
     let realMin = Math.floor((realSec - realHour * 3600) / 60);
     let realSeconds = realSec - realHour * 3600 - realMin * 60;
     setTime({ hour: realHour, minute: realMin, seconds: realSeconds });
+
+    if (realSec <= 0) {
+      clearInterval(intervalID.current);
+    }
   };
   console.log(time);
 
-  const stopClick = () => {};
-  const resetClick = () => {};
+  const stopClick = () => {
+    console.log("stop");
+    clearInterval(intervalID.current);
+  };
+  const resetClick = () => {
+    clearInterval(intervalID.current);
+    setLock(false);
+    setHour(0);
+    setMinute(0);
+    setSeconds(0);
+  };
 
   return (
     <>
       <h1>Count Down Timer</h1>
-
-      <input
-        onChange={hourChange}
-        type="number"
-        id="hour"
-        placeholder="00"
-      ></input>
-      <input
-        onChange={minuteChange}
-        type="number"
-        id="minute"
-        placeholder="00"
-      ></input>
-      <input
-        onChange={secondsChange}
-        type="number"
-        id="seconds"
-        placeholder="00"
-      ></input>
+      {lock === true ? (
+        <>
+          <span>{time.hour} :</span>
+          <span>{time.minute} : </span>
+          <span>{time.seconds}</span>
+        </>
+      ) : (
+        <>
+          <input
+            onChange={hourChange}
+            type="number"
+            id="hour"
+            placeholder="00"
+          ></input>
+          <input
+            onChange={minuteChange}
+            type="number"
+            id="minute"
+            placeholder="00"
+          ></input>
+          <input
+            onChange={secondsChange}
+            type="number"
+            id="seconds"
+            placeholder="00"
+          ></input>
+        </>
+      )}
 
       <div>
         <button onClick={startClick}>start</button>
